62d856ebc019584b1b1c9de7cc5789d838ef4386
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2022-09-05 17:47:00 -0300

    Inputs

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -6,7 +6,8 @@
         <input type="text" class=" mt-2
         form-control
         block
-        w-full
+        mx-auto
+        w-5/6
         px-3
         py-1.5
         text-base
@@ -20,6 +21,9 @@
         m-0
         focus:text-gray-700 focus:bg-white focus:border-yellow-600 focus:outline-none
       " id="exampleFormControlInput1" placeholder="Ex. Katy Perry" />
+        <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light"
+          class="inline-block px-6 mt-5 py-2.5 bg-yellow-600 w-5/6 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-700 hover:shadow-lg focus:bg-yellow-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Click
+          me</button>
       </div>
     </div>
   </div>
